3e4704c0fe45053fd098f72da2b21a9266cb7587
flexboni <kgb8594@gmail.com>
2020-10-06 14:46:36 +0900

    (Not Solved)11.3 Photo Navigation

--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -1,12 +1,26 @@
 import React from "react";
 import { View, Text } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
 import Home from "../screens/Home";
 import Search from "../screens/Search";
 import Notifications from "../screens/Notifications";
 import Profile from "../screens/Profile";
+import PhotoNavigation from "../navigation/PhotoNavigation";
 
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+function Add() {
+  return (
+    <Stack.Navigator headerMode="none">
+      <Stack.Screen
+        name="PhotoNavigation"
+        component={PhotoNavigation}
+      ></Stack.Screen>
+    </Stack.Navigator>
+  );
+}
 
 export default () => (
   <Tab.Navigator>
@@ -15,13 +29,19 @@ export default () => (
     <Tab.Screen
       name="Add"
       component={View}
-      listeners={({ navigation }) => ({
+      // options={{
+      //   tabPress: (navigation) => {
+      //     e.preventDefault();
+      //   },
+      // }}
+      listeners={({ navigation, route }) => ({
         tabPress: (e) => {
           // Prevent default action
           e.preventDefault();
 
           // Do something with the `navigation` object
-          navigation.navigate("PhotoNavigation");
+          // navigation.navigate(PhotoNavigation);
+          console.log("navigation :" + navigation.navigate("PhotoNavigation"));
         },
       })}
     />
